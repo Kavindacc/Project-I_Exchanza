6650dcf0b018565bf8830474f8f0d5f044da11af
Geethbim <geethbimsaraliyanage@gmail.com>
2024-10-21 02:21:35 +0530

    10-21 02.21

--- control/wishlistcon.php
+++ control/wishlistcon.php
@@ -4,12 +4,12 @@ include_once '../model/wishlist.php';
 session_start();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    $itemid = htmlspecialchars(trim($_POST['itemid']));
+    $productid = htmlspecialchars(trim($_POST['productid']));
     $userid = htmlspecialchars(trim($_POST['userid']));
     $cat=htmlspecialchars(trim($_POST['cat']));
     $sub=htmlspecialchars(trim($_POST['sub']));
 
-    if (!empty($itemid) && !empty($userid) && !empty($cat) && !empty($sub)) {
+    if (!empty($productid) && !empty($userid) && !empty($cat) && !empty($sub)) {
         //add wishlish table
         //check already add
         $dsn = new DbConnector();
@@ -17,10 +17,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         $item = new wishlist();
         $item->setUserId($userid);
-        $item->setItemId($itemid);
+        $item->setItemId($productid);
 
         $itemids = $item->getwishlistid($con);
-        if (in_array($itemid, $itemids)) {
+        if (in_array($productid, $itemids)) {
             $_SESSION['amsg'] = "Product already add to Wishlist.";
             header("Location: ../view/item_template.php?cat=$cat&sub=$sub");
             exit();
